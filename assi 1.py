29b40952a83ad2f9dea02218771dc9294796ae82
mahmoudalshannat95-code <mahmoud.alshannat95@gmail.com>
2026-05-10 13:28:35 +0300

    Add files via upload

---
+++ assi 1.py
@@ -0,0 +1,43 @@
+print('Wellcome In Mahmoud Alshannat Restorentoo ^_^')
+coffee=25
+water=10
+cake=40
+coffee*=2
+water*=3
+total_bill=(coffee + cake + water)
+print(total_bill)
+print(total_bill>100)
+print(total_bill==120)
+print('Thank You To Visit Mahmoud Alshannat\U0001F60A Restorentoo ')
+coffee=25
+water=10
+cake=40
+coffee+=5
+coffee*=2
+water*=3
+total_bill=(coffee + cake + water)
+print(total_bill)
+print(total_bill>100)
+print(total_bill==120)
+print('Thank You To Visit Mahmoud Alshannat Restorentoo\U0001F60D\U0001F60D\U0001F60D')
+print('......................')
+points=40
+points+=20
+points-=10
+points*=2
+print(points)
+is_customer=VIP=True
+print(is_customer==points>=100)
+condtion=total_bill>150 or is_customer==VIP
+print(condtion)
+result=10+5*2
+print(result)
+result=(10+5)*2
+print(result)
+print(True or False and False )
+print((True or False) and False )
+today_bill=120
+points=20
+premium_member=True
+print(today_bill>150 and points>50 or premium_member)
+print(today_bill>150 and (points>50 or premium_member))
